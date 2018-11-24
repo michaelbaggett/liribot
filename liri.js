bd32576d8ee5a0101dd300c43358c1cc685dec7c
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-11-24 14:53:47 -0600

    concert-this function added, currently displays venue, city, and date of next two shows, the date is not converted by momentum yet

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const Spotify = require("node-spotify-api");
-const spotifyKeys = require("./keys.js");
+const keys = require("./keys.js");
 const axios = require("axios");
 
 let userCommand = process.argv[2];
@@ -8,15 +8,19 @@ let userSearch = process.argv[3].slice();
 
 
 function spotifySearch() {
-    var spotify = new Spotify(spotifyKeys);
-       
-      spotify.search({ type: 'track', query: 'userSearch' }, function(err, data) {
+    var spotify = new Spotify(keys);
+
+    spotify.search({
+        type: 'track',
+        query: 'userSearch'
+    }, function (err, data) {
         if (err) {
-          return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
         }
-       
-      console.log(data); 
-      })};
+
+        console.log(data);
+    })
+};
 
 function omdbSearch() {
     let queryURL = "http://www.omdbapi.com/?t=" + userSearch + "&y=&plot=short&apikey=trilogy"
@@ -41,9 +45,30 @@ ${"Actors: " + data.Actors}
     )
 }
 
-// function bandsInTownSearch(){
+function bandsInTownSearch() {
+    let bandsURL = "https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp"
 
-// }
+
+    axios.get(bandsURL).then(
+        function (response) {
+            let venue1 = response.data[0].venue.name;
+            let venue2 = response.data[1].venue.name;
+            let city1 = response.data[0].venue.city;
+            let city2 = response.data[1].venue.city;
+            let time1 = response.data[0].datetime;
+            let time2 = response.data[1].datetime;
+            console.log(
+`
+Venue: ${venue1} 
+City: ${city1} 
+Date: ${time1},
+Venue: ${venue2} 
+City: ${city2} 
+Date: ${time2}
+`)
+        }
+    )
+}
 
 switch (userCommand) {
     case "movie-this":
